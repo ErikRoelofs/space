5a3810ff507bf9cf3dd01552fcd3425b4e151efd
Erik Roelofs <erik@ruigekonijnen.nl>
2017-01-12 22:33:16 +0100

    Rendering ships in the right color

--- game/directives/piece/directive.js
+++ game/directives/piece/directive.js
@@ -10,6 +10,16 @@ angular.module('game').directive('piece', ['$http', 'pieceTypesService', functio
             scope.img = function() {
                 return scope.pieceType.name.toLowerCase();
             }
+            scope.color = function() {
+                switch( parseInt(scope.piece.ownerId, 10) ) {
+                    case 1: return '#ff0000';
+                    case 2: return '#00ff00';
+                    case 3: return '#0000ff';
+                    case 4: return '#ff00ff';
+                    case 5: return '#ffff00';
+                    case 6: return '#00ffff';
+                }
+            }
         }
 
     }
