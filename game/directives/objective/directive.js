e2c8fc111de910b36dfd620a7643c33753f03613
Erik Roelofs <eroelofs@zigwebsoftware.nl>
2017-08-17 18:07:31 +0200

    Made the objectives look better

--- game/directives/objective/directive.js
+++ game/directives/objective/directive.js
@@ -1,25 +1,86 @@
-angular.module('game').directive('objective', [ '$http', '$rootScope',
-    function($http, $rootScope) {
-    return {
-        restrict: 'E',
-        scope: {
-            objective: '='
-        },
-        templateUrl: "directives/objective/template.html",
-        link: function(scope) {
+angular.module('game').directive('objective', ['$http', '$rootScope', 'playersService', 'orderService',
+    function ($http, $rootScope, playersService, orderService) {
+        return {
+            restrict: 'E',
+            scope: {
+                objective: '='
+            },
+            templateUrl: "directives/objective/template.html",
+            link: function (scope) {
 
-            scope.claim = function() {
-                var player = $rootScope.playerId;
-                var order = {
-                    objectiveId: scope.objective.id
+                scope.canClaim = true;
+                scope.beingClaimed = false;
+                angular.forEach(orderService.getCurrentOrdersForPlayer($rootScope.playerId), function(order) {
+                    if(order.orderType == 'claimObjective') {
+                        scope.canClaim = false;
+                        if(order.data.objectiveId == scope.objective.id) {
+                            scope.beingClaimed = true;
+                        }
+                    }
+                });
+
+                scope.claim = function () {
+                    var player = $rootScope.playerId;
+                    var order = {
+                        objectiveId: scope.objective.id
+                    };
+
+                    $http.post('/order/' + player + '/place/claimObjective', order).then(function (response) {
+                    }, function (error) {
+                        console.log(error);
+                    });
+
                 };
 
-                $http.post('/order/' + player + '/place/claimObjective', order).then(function(response) {
-                    console.log(response);
-                },function(error) {
-                    console.log(error);
-                })
-            };
+                var describeResourceObjective = function (objective) {
+                    var res = '';
+                    switch (objective.params.resource) {
+                        case 'social':
+                            res = 'Influence';
+                            break;
+                        case 'industry':
+                            res = 'Industry';
+                            break;
+                        default:
+                            res = 'Unknown Resource';
+                            break;
+                    }
+                    return 'I possess ' + objective.params.amount + ' unspent ' + res;
+                };
+
+                var describePieceObjective = function (objective) {
+                    var piece = '';
+                    switch (objective.params.type) {
+                        case 5:
+                            piece = 'Spacedocks';
+                            break;
+                        case 1:
+                            piece = 'Planets';
+                            break;
+                        default:
+                            piece = 'Unknown pieces?';
+                            break;
+                    }
+                    ;
+                    return 'I control ' + objective.params.amount + ' ' + piece + ' on the board.';
+                };
+
+
+                scope.describeObjective = function (objective) {
+                    switch (objective.type) {
+                        case 'has.resource':
+                            return describeResourceObjective(objective);
+                        case 'has.pieces':
+                            return describePieceObjective(objective);
+                        default:
+                            return 'Unknown type of objective?!';
+                    }
+                };
+
+                scope.getPlayerColor = function(playerId) {
+                    return playersService.getPlayer(playerId).color;
+                }
+
+            }
         }
-    }
-}]);
+    }]);
