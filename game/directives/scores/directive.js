400fea28ae0d0c39c9e8489d98d6a174b79cd134
Erik Roelofs <eroelofs@zigwebsoftware.nl>
2017-08-18 10:04:27 +0200

    Minor style updates to VP tracker

---
+++ game/directives/scores/directive.js
@@ -0,0 +1,21 @@
+angular.module('game').directive('scores', ['playersService', 'gameService', function(playersService, gameService) {
+    return {
+        restrict: 'E',
+        scope: {
+            game: '@'
+        },
+        templateUrl: "directives/scores/template.html",
+        link: function(scope) {
+            scope.players = playersService.getPlayers();
+            scope.scores = [];
+            scope.vpLimit = gameService.getGame().vpLimit;
+            for( var i = 0; i <= scope.vpLimit ; i++ ) {
+                scope.scores.push({
+                    score: i,
+                    players: scope.players.filter(function(player) { return player.score == i || ( i == scope.vpLimit && player.score > i ) ; })
+                });
+            }
+        }
+
+    }
+}]);
